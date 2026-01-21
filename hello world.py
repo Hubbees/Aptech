422eb02e486a3aa723b38d7427f656ffb7fc3c7e
unknown <nsyboss06@gmail.com>
2026-01-21 11:15:55 +0100

    Lists and Tuples

--- hello world.py
+++ hello world.py
@@ -1,1 +1,41 @@
-print("hello world!!!")
+print("Hello World")
+f = [0,1,2,3,4,5,6,16,8]
+a = 8
+b = 7.5
+c = 0.5
+d = "The Answer is "
+h = "64"
+e = a + b + c
+g = b + c
+i = 1
+j = 0
+k = 'n'
+l = " "
+listM = ['man', 'goat', 'foolish']
+N = [8, 16, 10, 9, 14, 15, 12]
+f.sort(reverse=True)
+print(d + str(e))
+print(f[-4] + f[-6] + f[-1])
+print (f[-9])
+print(g)
+print(b + c)
+print(e / a)
+print(e * g)
+print(int(h))
+print(bool(i))
+print(bool(j))
+print(bool(k))
+print(bool(l))
+print(len(listM))
+print(len(f))
+print(max(f))
+print(max(listM))
+print(f)
+f.sort()
+print(f)
+N.append(13)
+print(N)
+N.insert(3,7)
+print(N)
+N.pop(2)
+print(N)
